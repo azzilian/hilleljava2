9fba24ffd27cbe577664c3192ec5a3b918adee71
Vlad <crusotdeath@gmail.com>
2022-01-16 23:24:12 +0200

    Вот теперь создали матрицу

--- src/com/company/Homework4.java
+++ src/com/company/Homework4.java
@@ -5,48 +5,49 @@ import java.util.Scanner;
 
 public class Homework4 {
     public static void main(String[] args) {
-        int matrixSize = 0;
-        boolean enterExit = false;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите Размер матрицы: ");
-        do {
+        System.out.println("Начнём создание матрицы");
+        System.out.println("Строк " + enterRows());
+        System.out.println("Столбцов " + enterColums());
 
-            if (scanner.hasNextInt()){
-                matrixSize = scanner.nextInt();
-                if (matrixSize > 0) {
-                    enterExit = true;} else {
-                    System.out.println("Нужно ввести целое число больше 0"); //вот тут странный момент иногда возникает - дублируется сообщение если вводить некорректные данные, но не всегда, не пойму в чем дело
+
+    }
+
+    //Ввод количества строк
+    public static int enterRows () {
+            int a;
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите количество строк:");
+            if (scanner.hasNextInt()) {
+                a = scanner.nextInt();
+                if (a > 1){
+                    return a;
+                } else{
+                    System.out.print("Вы ввели слишком маленькое значение должно быть больше 1, попробуйте ещё раз ");
+                    return enterRows();
                 }
             } else {
-                System.out.println("Нужно ввести целое число больше 0");
-                scanner.nextLine();
+                System.out.print("Вы ввели некорректные данные,нужно целое число больше 1, давайте попробуем ещё раз ");
+                return enterRows();
             }
-        } while (!enterExit);
-        scanner.close();
-
-        double [][] squareMatrix = new double[matrixSize][matrixSize];
-        Random random = new Random();
-        double a =-50.00;
-        double b = 50.00;
-//      Заполняем массив
-        System.out.println("Наша матрица выглядит так");
-        for (int i = 0; i < squareMatrix.length; i++){
-            for (int j = 0; j< squareMatrix[i].length; j++){
-                squareMatrix[i][j] = a + random.nextDouble() * (b - a);
-            System.out.printf("%.2f   ",squareMatrix[i][j]);
-            }
-            System.out.println();
-
         }
-        //Создание матрицы
-
-        //Выводим матрицу на экран
-
-        //Меняем местами элементы
-        //Выводим новую матрицу на экран
-
+    //Ввод количества столбцов
+    public static int enterColums () {
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество столбцов:");
+        if (scanner.hasNextInt()) {
+            a = scanner.nextInt();
+            if (a > 1){
+                return a;
+            } else{
+                System.out.print("Вы ввели слишком маленькое значение должно быть больше 1, попробуйте ещё раз ");
+                return enterColums();
+            }
+        } else {
+            System.out.print("Вы ввели некорректные данные,нужно целое число больше 1, давайте попробуем ещё раз ");
+            return enterColums();
+        }
     }
-    public static void creationMatrix (String[] args){
 
-    }
+
 }
